33514500631685c453109a17be177d9f31d002ea
Santosh Kumar <sntshkmr60@gmail.com>
2021-01-29 22:33:10 +0530

    Adds sorting functionality by proj name and person

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,6 +4,17 @@
 
     <v-container fluid class="my-5">
 
+      <v-layout row class="mb-3 pa-3">
+        <v-btn small text color="grey" @click="sortBy('title')">
+          <v-icon left small>folder</v-icon>
+          <span class="caption text-lowercase">By project name</span>
+        </v-btn>
+        <v-btn small text color="grey" @click="sortBy('person')">
+          <v-icon left small>person</v-icon>
+          <span class="caption text-lowercase">By person</span>
+        </v-btn>
+      </v-layout>
+
       <v-card flat class="pa-3" v-for="project in projects" :key="project.title">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
           <v-flex xs12 md6>
@@ -46,6 +57,12 @@ export default {
         { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
       ]
+    }
+  },
+  methods: {
+    sortBy(prop) {
+      console.log("sortBy called");
+      this.projects.sort((a, b) => a[prop] < b[prop] ? -1: 1)
     }
   }
 }
