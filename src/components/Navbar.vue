c841bbf2ab72961bfc472026d23169b9f3cef442
Santosh Kumar <sntshkmr60@gmail.com>
2021-01-29 17:02:00 +0530

    Adds Lists to the sidebar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,7 +13,16 @@
       </v-btn>
     </v-toolbar>
     <v-navigation-drawer v-model="drawer" app class="primary">
-      <p class="info">test</p>
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-action>
+            <v-icon>{{link.icon}}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-item class="white--text">{{link.text}}</v-list-item-item>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -21,7 +30,12 @@
   export default {
     data() {
       return {
-        drawer: false
+        drawer: false,
+        links: [
+          { icon: 'dashboard', text: 'Dashboard', route: '/'},
+          { icon: 'folder', text: 'My Projects', route: '/projects'},
+          { icon: 'person', text: 'Team', route: '/team'},
+        ]
       }
     }
   }
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,7 +4,6 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-  iconfont: 'md',
   theme: {
     primary: '#9652ff',
     success: '#3cd1c2',
