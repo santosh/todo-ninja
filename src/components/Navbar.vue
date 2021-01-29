0e37bcfe3346bc49d2b53aeeeb749d63d26518f0
Santosh Kumar <sntshkmr60@gmail.com>
2021-01-29 22:32:37 +0530

    Updates markup to latest Vue standards

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <v-toolbar flat app class="grey lighten-4">
+    <v-app-bar app flat class="grey lighten-4">
       <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
@@ -11,7 +11,7 @@
         <span>Sign Out</span>
         <v-icon right>exit-to-app</v-icon>
       </v-btn>
-    </v-toolbar>
+    </v-app-bar>
     <v-navigation-drawer v-model="drawer" app class="primary">
       <v-list>
         <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
@@ -19,7 +19,7 @@
             <v-icon>{{link.icon}}</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-item class="white--text">{{link.text}}</v-list-item-item>
+            <v-list-item-action-text class="white--text">{{link.text}}</v-list-item-action-text>
           </v-list-item-content>
         </v-list-item>
       </v-list>
