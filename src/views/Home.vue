7302ba39d4252ffd5f788b1723da0a6282f2e834
Santosh Kumar <sntshkmr60@gmail.com>
2021-01-29 11:41:23 +0530

    Try different color & text classes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis laborum, delectus nulla expedita tempore molestias non esse harum ducimus ad adipisci, doloremque aperiam, iste dolore quod ipsum sed odit laudantium!</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ab animi repudiandae blanditiis doloremque. Doloremque, laborum! Saepe laboriosam accusantium iure unde ea fugiat eos, commodi repellendus sed enim quos dolorum.</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller Display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading font-weight-bold">This is subheading</p>
+    <p class="caption">This is a caption</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
