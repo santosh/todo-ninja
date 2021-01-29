f82e55bd8bcd64af7dbddc6d2777d4bf03b470c8
Santosh Kumar <sntshkmr60@gmail.com>
2021-01-29 17:26:09 +0530

    Adds Router, add new components/views

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="dashboard">
+    <h1>Dashboard</h1>
+  </div>
+</template> 
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
---
+++ src/views/Team.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="team">
+    <h1>Team</h1>
+  </div>
+</template> 
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
