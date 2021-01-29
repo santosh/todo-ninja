faa16a99bc5d0b2a9e7f82728b378b9e42d693e5
Santosh Kumar <sntshkmr60@gmail.com>
2021-01-29 16:08:09 +0530

    Adds v-navigation-drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
     <v-toolbar flat app>
+      <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span>Ninja</span>
@@ -11,10 +12,17 @@
         <v-icon right>mdi-exit-to-app</v-icon>
       </v-btn>
     </v-toolbar>
+    <v-navigation-drawer v-model="drawer" app class="indigo">
+      <p>test</p>
+    </v-navigation-drawer>
   </nav>
 </template>
 <script>
   export default {
-    
+    data() {
+      return {
+        drawer: false
+      }
+    }
   }
 </script>
