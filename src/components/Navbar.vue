f25d3d5fe8fdefd700bf80c37aed6bfd52011c46
Santosh Kumar <sntshkmr60@gmail.com>
2021-01-30 10:12:15 +0530

    Adds avatar to Team page & near logout button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,6 @@
 <template>
   <nav>
+
     <v-app-bar app text class="grey lighten-4">
       <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase grey--text">
@@ -12,7 +13,17 @@
         <v-icon right>exit-to-app</v-icon>
       </v-btn>
     </v-app-bar>
+
+
     <v-navigation-drawer v-model="drawer" app class="primary">
+      <v-layout column align-center>
+        <v-flex class="mt-5">
+          <v-avatar size="100">
+            <img src="/avatar-1.png">
+          </v-avatar>
+          <p class="white--text subheading mt-1">The Net Ninja</p>
+        </v-flex>
+      </v-layout>
       <v-list>
         <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-item-action>
@@ -24,6 +35,7 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
+
   </nav>
 </template>
 <script>
