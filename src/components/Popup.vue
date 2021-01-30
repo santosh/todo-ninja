d977fdc861b59cbe92acbdf9057b10eb03e5ef7e
Santosh Kumar <sntshkmr60@gmail.com>
2021-01-30 12:56:40 +0530

    Adds form in New Project Dialog

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog v-model="dialog" width="600">
+  <v-dialog width="600">
     <template v-slot:activator="{ on, attrs }">
       <v-btn color="success" v-bind="attrs" v-on="on">Add new Project</v-btn>
     </template>
@@ -10,21 +10,34 @@
       </v-card-title>
 
       <v-card-text>
-        Please add a new project. 
+        <v-form class="px-3">
+          <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
+          <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+        </v-form>
       </v-card-text>
-
+      
       <v-divider></v-divider>
-
+      
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="primary" text @click="dialog = false">
-          I understood
-        </v-btn>
+        <v-btn class="success" text @click="submit">Add Project</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      title: '',
+      content: ''
+    }
+  },
+  methods: {
+    submit() {
+      console.log(this.title, this.content);
+    }
+  }
+};
 </script>
