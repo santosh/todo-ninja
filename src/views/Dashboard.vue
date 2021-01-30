a3fd22f82103d1f34848100833170f7ae1e02274
Santosh Kumar <sntshkmr60@gmail.com>
2021-01-30 20:49:27 +0530

    Loads data from Firebase

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -75,47 +75,15 @@
 </template>
 
 <script>
-// @ is an alias to /src
+import db from '@/fb';
+
 
 export default {
   name: 'Dashboard',
   components: {},
   data() {
     return {
-      projects: [
-        {
-          title: 'Design a new website',
-          person: 'The Net Ninja',
-          due: '1st Jan 2019',
-          status: 'ongoing',
-          content:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!',
-        },
-        {
-          title: 'Code up the homepage',
-          person: 'Chun Li',
-          due: '10th Jan 2019',
-          status: 'complete',
-          content:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!',
-        },
-        {
-          title: 'Design video thumbnails',
-          person: 'Ryu',
-          due: '20th Dec 2018',
-          status: 'complete',
-          content:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!',
-        },
-        {
-          title: 'Create a community forum',
-          person: 'Gouken',
-          due: '20th Oct 2018',
-          status: 'overdue',
-          content:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!',
-        },
-      ],
+      projects: [],
     };
   },
   methods: {
@@ -124,6 +92,20 @@ export default {
       this.projects.sort((a, b) => (a[prop] < b[prop] ? -1 : 1));
     },
   },
+  created() {
+    db.collection('projects').onSnapshot((res) => {
+      const changes = res.docChanges();
+
+      changes.forEach(change => {
+        if (change.type === 'added') {
+          this.projects.push({
+            ...change.doc.data(),
+            id: change.doc.id,
+          })
+        }
+      })
+    })
+  }
 };
 </script>
 
